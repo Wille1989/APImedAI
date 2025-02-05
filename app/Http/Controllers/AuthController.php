d4a592482b52e70f9b2beecf6e4297db6034b395
Wille1989 <W.Lundqvist@live.se>
2025-02-05 10:56:28 +0100

    added ValidatonException

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate
-        ([
+        $request->validate([
             'name' => 'requiered | string', 
             'email' => 'requiered | email | unique:users',
             'password' => 'requiered | confirmed | min:8',
@@ -25,11 +25,34 @@ class AuthController extends Controller
                 'password' => Hash::make($request->password),
             ]);
 
-            return response()->json(['message' => 'New user Registered'], 201);
+            return response()->json(['message' => 'New user registered'], 201);
 
         } catch (\Exception $e)
         {
             return response()->json(['message' => 'Registration failed'], 500);
         }
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([ 
+            'email' => 'requiered | email',
+            'password' => 'requiered',
+
+            try{
+                $user = User::where('email', $reqeust->email->first());
+
+                if(!$user || !Hash::check($request->password, $user->password)){
+                    throw ValidationException::withMessages([
+                        'message' => ['The provided credentials are incorrect'],
+                    ]);
+                }
+
+            } catch(\Exception $e){
+                return response()->json(['message'])
+            }
+        ])
+    }
+
+
 }
